36bba99cde89f7a0e8baf14e5c54ab946e815be8
Xavier-Platinum <kwislawrencekwis@gmail.com>
2024-11-09 18:52:55 +0100

    feat(): Register flow and update profile

--- src/modules/notifications/broadcast/broadcast.module.ts
+++ src/modules/notifications/broadcast/broadcast.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { BroadcastService } from './broadcast.service';
 import { BroadcastController } from './broadcast.controller';
+import { BroadcastRepository } from './entities/broadcast.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Broadcast, BroadcastSchema } from './entities/broadcast.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Broadcast.name, schema: BroadcastSchema },
+    ]),
+  ],
   controllers: [BroadcastController],
-  providers: [BroadcastService],
+  providers: [BroadcastService, BroadcastRepository],
+  exports: [BroadcastService],
 })
 export class BroadcastModule {}
